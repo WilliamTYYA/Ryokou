15bf353b4aee3f55e697665c5cd846569c50b51f
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-10-23 12:58:36 -0500

    Initial Commit

---
+++ RyokouTests/RyokouTests.swift
@@ -0,0 +1,16 @@
+//
+//  RyokouTests.swift
+//  RyokouTests
+//
+//  Created by Thiha Ye Yint Aung on 10/23/25.
+//
+
+import Testing
+
+struct RyokouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
